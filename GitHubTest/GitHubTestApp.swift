1729b7bb3cfd9c445a9947a6fe4291d88a2d036d
9dpbQ <159412888+9dpbQ@users.noreply.github.com>
2025-07-03 15:05:28 +0900

    Initial commit with source code

---
+++ GitHubTest/GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestApp.swift
+//  GitHubTest
+//
+//  Created by qdpb on 2025/07/03.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
